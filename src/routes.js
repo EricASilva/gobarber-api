afa0a31c791c4ebff2e9412b4dc8438bfb0d92e7
Eric Augusto da Silva <eric.silva.augusto@gmail.com>
2020-02-02 21:05:26 -0300

    completing module 02

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import SessionController from './app/controllers/SessionController';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// Middleware executa somente para as proximas rotas abaixo
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
