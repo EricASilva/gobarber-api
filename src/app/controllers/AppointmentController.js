0947e122db541247143379426bbdf8b34537a19f
Eric Augusto da Silva <eric.silva.augusto@gmail.com>
2020-02-11 22:14:56 -0300

    cancel appointment done

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
@@ -59,7 +59,7 @@ class AppointmentController {
 
     if (provider_id === req.userId) {
       return res
-        .status(402)
+        .status(401)
         .json({ error: 'You cant create an appointment with yourself' });
     }
 
@@ -114,6 +114,29 @@ class AppointmentController {
       user: provider_id,
     });
 
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: "You don't permission to cancel this appointment." });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
     return res.json(appointment);
   }
 }
