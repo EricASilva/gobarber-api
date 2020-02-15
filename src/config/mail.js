bfc910526904a4ab8e0bff4f20e9ca0ec33dafe6
Eric Augusto da Silva <eric.silva.augusto@gmail.com>
2020-02-15 10:17:35 -0300

    Queue jobs done

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false, // true somente SSL
+  auth: {
+    user: '148862a9553f57',
+    pass: 'a4de14d01343e8',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
