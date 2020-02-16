1f47daebef30578a0c97d642521eaf1e298d74ac
Eric Augusto da Silva <eric.silva.augusto@gmail.com>
2020-02-16 00:14:58 -0300

    conclusao do backend

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false, // true somente SSL
   auth: {
-    user: '148862a9553f57',
-    pass: 'a4de14d01343e8',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
